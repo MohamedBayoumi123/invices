d5c7c654f7015f5c09db80b24322788ac073f02a
bayo <alshahdcompany6@gmail.com>
2025-05-15 23:29:04 +0200

    creat All file

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- Footer opened -->
+	<div class="main-footer ht-40">
+		<div class="container-fluid pd-t-0-f ht-100p">
+			<span>Copyright © 2020 <a href="#">Bayo_Tech</a>. Designed by <a href="https://www.facebook.com/mastar.king.792">Mohamed_Bayoumi</a> All rights reserved.</span>
+		</div>
+	</div>
+<!-- Footer closed -->
